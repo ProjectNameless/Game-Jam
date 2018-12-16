3a9db96d1baf2cb870daebf0441fb61b2f4d05dd
VexeDev <44173192+VexeDev@users.noreply.github.com>
2018-12-15 21:49:36 -0500

    main menu
    added main menu and some basic options

---
+++ New Unity Project/Assets/Menu Assets/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject optionsMenu;
+    public GameObject mainMenu;
+
+    public AudioMixer audioMixer;
+
+    //start code
+    public void startGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //options menu code
+    public void openOptions ()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    //quit code
+    public void quitGame ()
+    {
+        Application.Quit();
+    }
+
+    //all of this is options menu stuff
+
+    //go back
+    public void backOptions ()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    //full screen toggle
+    public void toggleFullScreen ()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    //volume slider
+    public void changeVolume (float volume)
+    {
+        audioMixer.SetFloat("PrimeVolume", volume);
+    }
+}
